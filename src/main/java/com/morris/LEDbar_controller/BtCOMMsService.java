c14b7390200d293d0f1c5dbff0c3aefd121d0a4c
maurycyszpak <morris.starling@gmail.com>
2024-01-02 22:42:54 +0100

    Commit after refactoring ConstraintLayout from android.support to androidx packages. No functional code changes yet

--- src/main/java/com/morris/LEDbar_controller/BtCOMMsService.java
+++ src/main/java/com/morris/LEDbar_controller/BtCOMMsService.java
@@ -36,7 +36,6 @@
     import java.lang.reflect.InvocationTargetException;
     import java.sql.Timestamp;
     import java.text.SimpleDateFormat;
-    import java.util.Arrays;
     import java.util.Collections;
     import java.util.Date;
     import java.util.List;
